5639c8587ba2c41c2280588ab4e1b6001cda2116
Fred Oliveira <fred@helloform.com>
2026-06-01 16:17:06 +0100

    Updated speaker list

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -1,20 +1,40 @@
 export type Speaker = {
-	name: string;
-	role: string;
-	org: string;
-	topic: string;
-	profileUrl?: string;
-	orgUrl?: string;
+  name: string;
+  role: string;
+  org: string;
+  profileUrl?: string;
+  orgUrl?: string;
+  image?: string;
 };
 
 export const speakers: Speaker[] = [
-	{
-		name: "Steve Ruiz",
-		role: "Founder",
-		org: "TLDraw",
-		topic: "MC",
-		profileUrl: "https://x.com/steveruizok",
-		orgUrl: "https://tldraw.dev/",
-	},
-	{ name: "More", role: "Growth & Eng", org: "soon", topic: "" },
+  {
+    name: "Steve Ruiz",
+    role: "Founder",
+    org: "TLDraw",
+    profileUrl: "https://x.com/steveruizok",
+    orgUrl: "https://tldraw.dev/",
+    image: "steve.png",
+  },
+  {
+    name: "Ben Brandt",
+    role: "Creator of ACP",
+    org: "Zed Industries",
+    orgUrl: "https://zed.dev",
+    image: "ben.png",
+  },
+  {
+    name: "Matt Carey",
+    role: "",
+    org: "Cloudflare",
+    orgUrl: "https://cloudflare.com",
+    image: "matt.png",
+  },
+  {
+    name: "Duarte Carmo",
+    role: "",
+    org: "Independent",
+    orgUrl: "https://duarteocarmo.com/",
+    image: "duarte.png",
+  },
 ];
